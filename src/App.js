5065b23df395bc5490dd22112b3b28c9423ef500
AliAbdilahi <71363632+AliAbdilahi@users.noreply.github.com>
2022-12-05 08:54:27 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import PostForm from './components/PostForm';
+function App() {
+  return (
+    <div className="App">
+      <h1>Publish Your Project</h1>
+      <h4>*You have to fill every textfield inorder to publish your project</h4>
+        <PostForm />
+    </div>
+  );
+}
+
+export default App;
